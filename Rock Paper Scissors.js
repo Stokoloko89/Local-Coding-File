9a34bb48bf8b437c9ac9802fe1eccc1f4a53df6e
Stokoloko@89 <lungaqaba@gmail.com>
2021-12-24 09:48:26 +0200

    Completed Round Winner Function

--- Rock Paper Scissors.js
+++ Rock Paper Scissors.js
@@ -15,27 +15,46 @@ function getResult () {
     return result[indexValue];
 }  
 
+// This function evaluates the two inputs - from the player and rom the computer. It returns the winning hand. based on the rules of Rock, Paper, Scissors. 
 function showWinner(playerSelection, getResult) {
-    let firstChar = playerSelection[0].toUpperCase(); 
+    //To make the player input case insensitive, we need to identfy the first character of the string, and assign it to a the variable. 
+    let firstChar = playerSelection[0].toUpperCase();         
+    //then we need to separate the first character from the remaining characters and save the remaining characters to a new variable. We then lowerecase 
+    //the remaining characters to remove and uppercased characters.   
     let remaining = playerSelection.toLowerCase().slice(1);
+    //We then assign a the values of the above variables to the playerSelection variable  
+    //by combining both the uppercased first character and the remaining characters.
     playerSelection = firstChar.concat(remaining);
-    if (playerSelection === 'Paper' && getResult() === 'Rock') {
-        return ('You win! Paper beats Rock'); 
-    } else if 
-        (playerSelection === 'Rock' && getResult() === 'Scisssors') {
-            return ('You win! Rock beats Scissors')
-        } else if 
-            (playerSelection === 'Scissors' && getResult() === 'Paper') {
-                return ('You win! Scissors beats Paper')
-            } else if 
-                (playerSelection === 'Paper' && getResult() === 'Scissors') {
-                    return ('You lose! Scissors beats Paper')
-                } else if 
-                    (playerSelection === 'Rock' && getResult() === 'Paper') {
-                        return ('You lose! Paper beats Rock')
-                    } else if 
-                        (playerSelection === 'Scissors' && getResult() === 'Rock') {
-                            return ('You lose! Rock beats Scissors')
-                        }
-        }
-      
+    //Now we declare the rules to the computer using strick equality and the boolean operators. For the computers selection we refer to the getResult function.
+    if (playerSelection === getResult()) {
+        return ('Its a tie!');
+    }
+
+    if  (
+        (playerSelection === 'Paper' && getResult() === 'Rock') || 
+        (playerSelection === 'Rock' && getResult() === 'Scissors') ||
+        (playerSelection === 'Scissors' && getResult() === 'Paper')
+        ) {
+            return ('Player Wins!')
+    }
+    if (
+        (getResult() === 'Paper' && playerSelection === 'Rock') || 
+        (getResult() === 'Rock' && playerSelection === 'Scissors') ||
+        (getResult() === 'Scissors' && playerSelection === 'Paper') 
+        ) {
+            return ('Computer wins!')
+    }
+}
+    
+
+
+// Keeps score of a winner or loser.
+
+
+// You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else 
+// (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! 
+// Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
+// At this point you should be using console.log() to display the results of each round and the winner at the end.
+// Use prompt() to get input from the user. Read the docs here if you need to.
+// Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
+// Feel free to create more “helper” functions if you think it would be useful. */
